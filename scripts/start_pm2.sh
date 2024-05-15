dbb68347b3483e778fad2b268923b3c1626734a0
Graham Wakefield <grrrwaaa@gmail.com>
2024-05-15 15:23:56 -0400

    update

--- scripts/start_pm2.sh
+++ scripts/start_pm2.sh
@@ -4,5 +4,5 @@ cd /home/ubuntu/nodelab
 
 sudo pm2 stop server.js
 sudo pm2 delete 0
-sudo pm2 start server.js -o output.log -e error.log
+sudo pm2 start server.js -o public/output.log -e public/error.log
 sudo pm2 save
